0cc4ee95de794b3d714a7b04684920a43d88737d
gelo <gelodev83@gmail.com>
2025-03-18 23:18:06 +0800

    updated

--- src/components/sections/VideoView.tsx
+++ src/components/sections/VideoView.tsx
@@ -1,3 +1,4 @@
+import StatsSection from "../shared/StatBox";
 import VideoViewBox from "../shared/VideoViewBox";
 
 
@@ -5,6 +6,7 @@ export default function VideoView() {
   return (
     <div className="mt-20">
         <VideoViewBox/>
+        <div className="mt-20"><StatsSection/></div>
     </div>
   )
 }
---
+++ src/components/shared/StatBox.tsx
@@ -0,0 +1,28 @@
+export default function StatsSection() {
+  const stats = [
+    { number: "34", label: "Buildings Finished Now" },
+    { number: "12", label: "Years Experience" },
+    { number: "24", label: "Awwards Won 2023" },
+  ];
+  return (
+    <div className="flex flex-col items-center gap-12 ">
+      {stats.map((stat, index) => (
+        <div
+          key={index}
+          className="relative flex items-center justify-between bg-[#FFEEE9] p-5 py-6 rounded-lg shadow-md w-[17rem]"
+        >
+         
+          <div className="flex items-center gap-6 pl-5">
+            <span className="text-4xl font-bold text-main">
+              {stat.number}
+            </span>
+            <span className="text-myblack font-semibold w-[110px] ">{stat.label}</span>
+          </div>
+
+        
+          <div className="absolute right-[-10px] top-1/9 transform -translate-y-1/2 size-12 bg-main rounded-full"></div>
+        </div>
+      ))}
+    </div>
+  );
+}
